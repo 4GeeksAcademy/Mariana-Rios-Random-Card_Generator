f2c3fe27aa519474404dc362b46f2203a344bea6
mariana10rios <140272078+mariana10rios@users.noreply.github.com>
2023-09-11 16:24:33 +0000

    clean completed

--- src/app.js
+++ src/app.js
@@ -5,29 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// window.onload = function() {
-//   //write your code here
-//   let randSuit = Math.floor(Math.random() * 4);
-//   let randNum = Math.floor(Math.random() * 13);
-
-//   let valueArr = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-//   let cardValue = document.querySelector("#cardValue");
-//   cardValue.innerHTML = valueArr[randNum];
-
-//   let suitArr = ["♦", "♥", "♠", "♣"];
-//   let suitValue = document.querySelectorAll(".suitValue");
-//   // suitValue[0].innerHTML = suitArr[randSuit];
-//   // suitValue[1].innerHTML = suitArr[randSuit];
-//   //querySelectorAll returns node list so use (above stuff) or for each:
-//   suitValue.forEach(function(suit) {
-//     let suitVal = suitArr[randSuit];
-//     suit.innerHTML = suitVal;
-//     if (suitVal === "♦" || suitVal === "♥") {
-//       suit.style.color = "red";
-//     }
-//   });
-// };
-
 function generateCard() {
   //write your code here
   let randSuit = Math.floor(Math.random() * 4);
